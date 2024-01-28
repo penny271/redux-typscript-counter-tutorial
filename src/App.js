fc24bb2fe9434ddea485af64e3812bfbac2b6b72
penny271 <hirotaka.aoki27@gmail.com>
2024-01-28 16:14:05 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
+import { useState } from "react";
 
 function App() {
+  // reduxのアクセスするための hooks
+  // state.counter.value は、store.js を見ている
+  const count = useSelector((state) => state.counter.value);
+  // reduxのhooks 通知を出せる
+  const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState("2");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>count: {count}</h1>
+      <input
+        onChange={(e) => setIncrementAmount(e.target.value)}
+        value={incrementAmount}
+      ></input>
+      {/* storeに特定のreducerを使うよう通知を出している */}
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        追加
+      </button>
     </div>
   );
 }
---
+++ src/redux/counterSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// reducerを作成すると、自動でActionCreatorも作られる
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  // 関数のようなもの
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    // input要素で増減する値が固定されていない場合、action.payloadを使う
+    incrementByAmount: (state, action) => {
+      console.log("action :>> ", action);
+      // {type: 'counter/incrementByAmount', payload: 2}
+      state.value += action.payload; //2, 10, 100
+    },
+  },
+});
+
+// - 下記2つはセット
+// 個々のreducerを使えるようにする
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// このreducerをエクスポートすることで、ストア(store.js)のroot reducerに含めることができます。
+export default counterSlice.reducer;
